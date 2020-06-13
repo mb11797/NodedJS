829c344a7e6fb804f648c29ed3b7dded059ec993
Manas Bhardwaj <mb.manasbhardwaj@gmail.com>
2020-06-14 00:18:43 +0530

    sync programming paradigm

---
+++ 7/Raw/POC/async.js
@@ -0,0 +1,12 @@
+let fs = require("fs");
+console.log("Before");
+let content = fs.readFile("f1.txt", function(err, content){
+    if(err){
+        console.log("Error: ");
+        console.log(err);
+    }
+    else{
+        console.log(content + "");
+    }
+});
+console.log("After");
