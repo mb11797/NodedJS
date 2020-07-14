7d24b2376630ea5a88e8da5d4f7c9fe5c6dd2195
Manas Bhardwaj <mb.manasbhardwaj@gmail.com>
2020-07-14 19:13:22 +0530

    challenges creation successfull

--- 15/Fair/hackerrank.js
+++ 15/Fair/hackerrank.js
@@ -1,4 +1,5 @@
 // "url": "https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login"
+// node "hackerrank.js" "../credentials.json" "./questions.js" "keyidi9272"
 
 let fs = require("fs");
 require("chromedriver");
@@ -8,7 +9,8 @@ let bldr = new swd.Builder();
 let driver = bldr.forBrowser("chrome").build();
 
 let cFile = process.argv[2];
-let userToAdd = process.argv[3];
+// let userToAdd = process.argv[3];
+let questionsFile = process.argv[3];
 
 // //myFn() banao phir alag se call karo
 // async function myFn(){
@@ -21,52 +23,39 @@ let userToAdd = process.argv[3];
 //function banate hi usko call kar diya
 (async function(){
     try{
-        //selenium inbuilt
-        await driver.manage().setTimeouts({
-            implicit:10000,
-            pageLoad:10000,
-        })
-        // buffer credentials
-        let bCredentials = await fs.promises.readFile(cFile);
-        // let myCredentials = JSON.parse(bCredentials)[2];
-        let myCredentials = JSON.parse(bCredentials);
-        // console.log(myCredentials);
-        let user = myCredentials.user;
-        let pwd = myCredentials.password;
-        let url = myCredentials.url;
-
-        await driver.get(url);
-        // find input-1
-        const uNameInputPromise = driver.findElement(swd.By.css("#input-1"));
-
-        //find input-2
-        const pwdInputPromise = driver.findElement(swd.By.css("#input-2"));
-
-        let elements = await Promise.all([uNameInputPromise, pwdInputPromise]);
-        let uNameWillBeSentPromise = elements[0].sendKeys(user);
-        let pwdWillBeSentPromise = elements[1].sendKeys(pwd);
-        await Promise.all([uNameWillBeSentPromise, pwdWillBeSentPromise]);
-        let sbmtBtn = await driver.findElement(swd.By.css("button[data-analytics=LoginPassword]"));
-        await sbmtBtn.click();
-        console.log("User logged in");
+        await loginHelper();
+        //******************************Home page********************************
         let dropdown = await driver.findElement(swd.By.css("a[data-analytics=NavBarProfileDropDown]"));
         await dropdown.click();
         let adminBtn = await driver.findElement(swd.By.css("a[data-analytics=NavBarProfileDropDownAdministration]"))
         await adminBtn.click();
         console.log("Admin page reached");
-
+        
+        //******************************Manage challenges*************************
         await waitForLoader();
         let lis = await driver.findElements(swd.By.css(".administration header ul li"));
         await lis[1].click();
 
+        let managePageURL = await driver.getCurrentUrl();
+        let questions = require(questionsFile);
+
+        console.log(`No of challenges: ${questions.length}`);
+
+        for(let i=0; i<questions.length; i++){
+            await driver.get(managePageURL);
+            await waitForLoader();
+            await createChallenge(questions[i]);
+            console.log(`Challenge ${i}`);
+        }
+
         //manage contest, manage challenge
         //json file read
 
-        let createChallengeBtn = await driver.findElement(swd.By.css(".btn.btn-green.backbone.pull-right"))
-        await createChallengeBtn.click();
+        // let createChallengeBtn = await driver.findElement(swd.By.css(".btn.btn-green.backbone.pull-right"))
+        // await createChallengeBtn.click();
 
-        console.log("I was here");
-        await createChallenge();
+        // console.log("I was here");
+        // await createChallenge();
         // tags => write enter press
         // submit btn
     }
@@ -80,45 +69,78 @@ let userToAdd = process.argv[3];
 
 // NOTE - async fn ka sara code try catch me aayega
 
-async function createChallenge(){
-    // await waitForLoader();
-    // let problemStatementBox = await driver.findElement(swd.By.css("#problem_statement-container .CodeMirror textarea"));
-    // await problemStatementBox.click();
-    // await problemStatementBox.sendKeys("abc");
+async function createChallenge(question){
+    let createChallengeBtn = await driver.findElement(swd.By.css(".btn.btn-green.backbone.pull-right"))
+    await createChallengeBtn.click();
+    await waitForLoader();
 
-    let eLinkArr = ["#name", "textarea.description", "#problem_statement-container textarea", "#input_format-container textarea", "#constraints-container textarea", "#output_format-container textarea"];
+    let eLinkArr = ["#name", "textarea.description", "#problem_statement-container .CodeMirror div textarea", "#input_format-container .CodeMirror div textarea", "#constraints-container .CodeMirror div textarea", "#output_format-container .CodeMirror div textarea", ".tagsinput input"];
 
-    let elementsArrayPromise = eLinkArr.map(function(link){
-        return driver.findElement(swd.By.css(link));
+    let elementsArrayPromise = eLinkArr.map(function(selector){
+        return driver.findElement(swd.By.css(selector));
     })
 
     let elementsArray = await Promise.all(elementsArrayPromise);
-    console.log(elementsArray.length);
-    let dataSentPromise = elementsArray.map(function(element){
-        return element.sendKeys("abc");
-    })
+    
+    await elementsArray[0].sendKeys(question["Challenge Name"]);
+    await elementsArray[1].sendKeys(question["Description"]);
+    await handleContainer("#problem_statement-container .CodeMirror div", elementsArray[2], question["Problem Statement"]);
+    await handleContainer("#input_format-container .CodeMirror div", elementsArray[3], question["Input Format"]);
+    await handleContainer("#constraints-container .CodeMirror div", elementsArray[4], question["Constraints"]);
+    await handleContainer("#output_format-container .CodeMirror div", elementsArray[5], question["Output Format"]);
+    await elementsArray[6].sendKeys(question["Tags"]);
+    await elementsArray[6].sendKeys(swd.Key.ENTER);
+    let saveBtn = await driver.findElement(swd.By.css(".save-challenge.btn.btn-green"));
+    await saveBtn.click();
 
-    await Promise.all(dataSentPromise);
-    console.log("Text has been added");
-
-    // // challenge Name => id => #name
-    // let challengeBoxPromise = driver.findElement(swd.By.css("#name"));
-    // // description => textarea.description
-    // let descBoxPromise = driver.findElement(swd.By.css("textarea.description"));
-    // // problem statement => #problem_statement-container textarea
-    // let problemStatementBoxPromise = driver.findElement(swd.By.css("#problem_statement-container textarea"));
-    // // input format => #input_format-container textarea
-    // let inputFormatBoxPromise = driver.findElement(swd.By.css("#input_format-container textarea"));
-    // // constraints => #constraints-container textarea
-    // let constraintsBoxPromise = driver.findElement(swd.By.css("#constraints-container textarea"));
-    // // output format => #output_format-conatiner textarea
-    // let outputFormatBoxPromise = driver.findElement(swd.By.css("#output_format-container textarea"));
-
-
+    // jugaad => parent height
+    // div => height => increase (JS execute browser)
+    // await driver.executeScript("alert('Amazing'");
+    // DOM => dynamically page load content change
 }
 
 
 async function waitForLoader(){
     let loader = await driver.findElement(swd.By.css("#ajax-msg"));
     await driver.wait(swd.until.elementIsNotVisible(loader));
+}
+
+async function loginHelper(){
+    //selenium inbuilt
+    await driver.manage().setTimeouts({
+        implicit:10000,
+        pageLoad:10000,
+    })
+    // buffer credentials
+    let bCredentials = await fs.promises.readFile(cFile);
+    // let myCredentials = JSON.parse(bCredentials)[2];
+    let myCredentials = JSON.parse(bCredentials);
+    // console.log(myCredentials);
+    let user = myCredentials.user;
+    let pwd = myCredentials.password;
+    let url = myCredentials.url;
+
+    await driver.get(url);
+    // find input-1
+    const uNameInputPromise = driver.findElement(swd.By.css("#input-1"));
+
+    //find input-2
+    const pwdInputPromise = driver.findElement(swd.By.css("#input-2"));
+
+    let elements = await Promise.all([uNameInputPromise, pwdInputPromise]);
+    let uNameWillBeSentPromise = elements[0].sendKeys(user);
+    let pwdWillBeSentPromise = elements[1].sendKeys(pwd);
+    await Promise.all([uNameWillBeSentPromise, pwdWillBeSentPromise]);
+    let sbmtBtn = await driver.findElement(swd.By.css("button[data-analytics=LoginPassword]"));
+    await sbmtBtn.click();
+    console.log("User logged in");
+}
+
+// pSelector => parentSelector
+async function handleContainer(pSelector, container, data){
+    // JS parent => height change
+    let parentElement = await driver.findElement(swd.By.css(pSelector));
+    await driver.executeScript("arguments[0].style.height='10px'", parentElement);
+    // send keys
+    await container.sendKeys(data);
 }
