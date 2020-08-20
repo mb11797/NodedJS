a5c39fa4b41b3703508b63dce94d2fe99fd59d01
Manas Bhardwaj <mb.manasbhardwaj@gmail.com>
2020-08-20 13:54:16 +0530

    electron environment created and required window created using it

--- 20/activity/main.js
+++ 20/activity/main.js
@@ -3,6 +3,7 @@
 // 2. cd => main.js
 // 3. npm init -y
 // 4. npm install electron --save-dev
+// 5. npm install --save-dev jquery
 
 // To run:
 // npm start
@@ -23,30 +24,30 @@ const ejs = require("ejs-electron");
 ejs.data({
   "title": "My Excel 1.0",
   "rows": 100,
-  "cols":26
+  "cols": 26
 })
 
-function createWindow(){
-    const win = new electron.BrowserWindow({
-        width: 800,
-        height: 600,
-        //to open app in hidden 
-        show: false,
-        webPreferences:{
-            nodeIntegration: true
-        }
-    });
+function createWindow() {
+  const win = new electron.BrowserWindow({
+    width: 800,
+    height: 600,
+    //to open app in hidden 
+    show: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
-    win.loadFile("index.ejs").then(function(){
-        //to remove default menu
-        win.removeMenu();
-        //maximize
-        win.maximize();
-        //unhide app so that user could view it
-        win.show();
-        //to open dev tools
-        win.webContents.openDevTools();
-    });
+  win.loadFile("index.ejs").then(function () {
+    //to remove default menu
+    win.removeMenu();
+    //maximize
+    win.maximize();
+    //unhide app so that user could view it
+    win.show();
+    //to open dev tools
+    win.webContents.openDevTools();
+  });
 }
 
 // app.whenReady() => package kardega app ko => desktop open => by calling createWindow
@@ -57,18 +58,18 @@ app.whenReady().then(createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  })
-  
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-  
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
   // In this file you can include the rest of your app's specific main process
   // code. You can also put them in separate files and require them here.
